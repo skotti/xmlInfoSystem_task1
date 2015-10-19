1aff009250d4c8e9e5397edf53f91a42707562e0
skotti <anastasiya.ruznaskaya@frtk.ru>
2015-10-19 19:14:27 +0400

    Also the test main was changed for some time

--- testClass.java
+++ testClass.java
@@ -5,10 +5,36 @@
  */
 package xml_netk;
 
+import java.io.File;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
 /**
  *
  * @author mac
  */
 public class testClass {
+    
+    public static void main(String[] args) {
+        
+    File newFile = new File("/Users/mac/NetBeansProjects/xml_netk/src/xml_netk/source.xml");
+    
+    xmlDocImpl xm = new xmlDocImpl();
+    
+    System.out.println(xm.initObject(newFile));
+    
+    //System.out.println(xm.getRootName());
+    
+    //NodeList el = xm.findObject(10);
+    
+    xm.deleteObject(50);
+   
+    //String[] atributes = new String[3];
+    /*atributes[0] = "deptno";
+    atributes[1] = "1555";
+    atributes[2] = "employee";*/
+    
+    //xm.addObject(atributes);
+    }
     
 }
